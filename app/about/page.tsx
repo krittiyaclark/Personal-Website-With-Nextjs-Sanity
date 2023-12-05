52ec45c76d4212ea5de936f4283ec820ab5a4b18
krittiyaclark <krittiyaclark@gmail.com>
2023-12-04 22:54:40 -0500

    Project setup

---
+++ app/about/page.tsx
@@ -0,0 +1,6 @@
+import { NextStudio } from "next-sanity/studio";
+import config from "@/sanity.config";
+
+export default function About() {
+  return <h1>About</h1>;
+}
---
+++ sanity.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+
+const config = defineConfig({
+  projectId: "blgwm78i",
+  dataset: "production",
+  title: "My Personal Project",
+  apiVersion: "2023-11-04",
+  basePath: "/admin",
+  plugins: [deskTool()],
+});
+
+export default config;
