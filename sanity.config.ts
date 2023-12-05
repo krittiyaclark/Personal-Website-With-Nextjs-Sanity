0e112a52aba1b580185c2cd1d65734d464f49a9b
krittiyaclark <krittiyaclark@gmail.com>
2023-12-05 12:32:10 -0500

    Setup schemas

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
+import schemas from "./sanity/schemas";
 
 const config = defineConfig({
   projectId: "blgwm78i",
@@ -8,6 +9,7 @@ const config = defineConfig({
   apiVersion: "2023-11-04",
   basePath: "/admin",
   plugins: [deskTool()],
+  schema: { types: schemas },
 });
 
 export default config;
